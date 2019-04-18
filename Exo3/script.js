e897450564646646cb1e035dcff66e5e64017ce7
Pokmand <manudesj@msn.com>
2019-04-18 14:24:16 +0200

    exo 3 ok avec modif click

--- Exo3/script.js
+++ Exo3/script.js
@@ -1,5 +1,5 @@
 $(function() {
-  $("#check").click(function() {
+
     var nom = $("#lastname").val();
     var prenom = $("#firstname").val();
     var email = $("#email").val();
@@ -10,28 +10,29 @@ $(function() {
 
     $("#lastname").focusout(function() {
       var nom = $("#lastname").val();
-      if (regextext.test(nom)) {} else {
+      if (!regextext.test(nom)) {
         swal.fire("NOM : Merci d'utiliser uniquement des lettres et des tirets");
       }
     })
     $("#firstname").focusout(function() {
       var prenom = $("#firstname").val();
-      if (regextext.test(prenom)) {} else {
+      if (!regextext.test(prenom)) {
         swal.fire(" PRENOM : Merci d'utiliser uniquement des lettres et des tirets");
       }
     })
     $("#email").focusout(function() {
       var email = $("#email").val();
-      if (regexmail.test(email)) {} else {
+      if (!regexmail.test(email)) {
         swal.fire("EMAIL : Merci de saisir uniquement une adresse email");
       }
     })
     $("#phoneNumber").focusout(function() {
       var telephone = $("#phoneNumber").val();
-      if (regextel.test(telephone)) {} else {
+      if (!regextel.test(telephone)) {
         swal.fire("TELEPHONE : Merci de saisir uniquement un numéro de portable");
       }
     })
+    $("#check").click(function() {
     if (regextext.test(nom) && regextext.test(prenom) && regextel.test(telephone) && regexmail.test(email))
       swal.fire("Je m'appelle " + prenom + " " + nom + "\n Mon email est : " + email + " et mon téléphone est : " + telephone + ".");
 
